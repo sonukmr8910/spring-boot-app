7a0727726a04ff284e4e310a32579c675654000b
Sonu Kumar K <sonukmr8910@gmail.com>
2021-11-20 10:18:28 +0530

    initial commit

---
+++ src/main/java/com/sonu/sonuapp/SonuAppApplication.java
@@ -0,0 +1,13 @@
+package com.sonu.sonuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonu/sonuapp/SonuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonu.sonuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
